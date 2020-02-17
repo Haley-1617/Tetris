7e458ec09934afb5ef2c8ac4031ec01f848de2f9
Haley-1617 <alicelai2920@gmail.com>
2020-02-17 12:13:17 -0800

    Initial Commit

---
+++ Tetris/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetris
+//
+//  Created by Haley Lai on 2/17/20.
+//  Copyright © 2020 Haley Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
